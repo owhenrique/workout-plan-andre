5c29d86db9a520db9c2c8cbe15eab75c88ee7155
owhenrique <me.pauloalmeida@gmail.com>
2024-05-27 19:15:14 -0300

    feat: change student workout

--- src/mocks/treinoC.ts
+++ src/mocks/treinoC.ts
@@ -4,7 +4,7 @@ const treinoC: Treino = {
     categoria: "Peito, Ombros e Tríceps",
     exercicios: [
       { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2023/09/smith-bench-press.gif", nome: "Supino reto Smith", series: 3, repeticoes: 12, descanso: 60 },
-      { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-inclinado-com-barra.gif", nome: "Supino inclinado c/ barra", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.lyfta.app/_next/image?url=https%3A%2F%2Flyfta.app%2Fimages%2Fexercises%2F07571101.png&w=640&q=10", nome: "Supino inclinado Smith", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2023/08/cable-middle-fly.gif", nome: "Crucifixo em pé", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/ombros-elevacao-lateral-de-ombros-com-halteres.gif", nome: "Elevação lateral", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/desenvolvimento-para-ombros-com-halteres.gif", nome: "Desenvolvimento sentado", series: 3, repeticoes: 12, descanso: 60 },
