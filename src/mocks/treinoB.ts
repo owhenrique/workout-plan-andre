59eab9aa30dc254281c0d1c3eea8c0581a1ea03f
owhenrique <me.pauloalmeida@gmail.com>
2024-05-27 19:05:49 -0300

    feat: change student workouts

--- src/mocks/treinoB.ts
+++ src/mocks/treinoB.ts
@@ -3,11 +3,11 @@ import { Treino } from "@/interfaces/types";
 const treinoB: Treino = {
     categoria: "Pernas",
     exercicios: [
-      { imagem: "https://fitnessprogramer.com/wp-content/uploads/2021/08/Lever-Horizontal-Leg-Press.gif", nome: "Leg press", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://alcateiademonstros.com.br/wp-content/uploads/2022/09/Avanco-com-halter.gif", nome: "Afundo c/ halteres", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://media.tenor.com/bqKtsSuqilQAAAAM/gym.gif", nome: "Cadeira extensora", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://media.tenor.com/ZElx6PviDq0AAAAM/gym.gif", nome: "Cadeira flexora", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2017/11/smith-full-squat.gif", nome: "Agachamento Smith", series: 3, repeticoes: 12, descanso: 60 },
-      { imagem: "https://www.blog.treinoemalta.com.br/wp-content/uploads/2023/07/sentado.gif", nome: "Panturilha sentado", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.lyfta.app/thumbnails/11641201.jpg", nome: "Panturilha Smith", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/corrida-na-esteira-inclinada-1.gif", nome: "Esteira", duracao: 15 }
     ]
   };
--- src/mocks/treinoC.ts
+++ src/mocks/treinoC.ts
@@ -3,7 +3,7 @@ import { Treino } from "@/interfaces/types";
 const treinoC: Treino = {
     categoria: "Peito, Ombros e Tríceps",
     exercicios: [
-      { imagem: "https://s6.gifyu.com/images/supino-reto_na-maquina.gif", nome: "Supino máquina", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2023/09/smith-bench-press.gif", nome: "Supino reto Smith", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-inclinado-com-barra.gif", nome: "Supino inclinado c/ barra", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2023/08/cable-middle-fly.gif", nome: "Crucifixo em pé", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/ombros-elevacao-lateral-de-ombros-com-halteres.gif", nome: "Elevação lateral", series: 3, repeticoes: 12, descanso: 60 },
