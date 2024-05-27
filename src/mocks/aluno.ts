766b96832b937f48725c1ad538007cc017b0cdb9
owhenrique <me.pauloalmeida@gmail.com>
2024-05-27 18:55:00 -0300

    feat: add project archtecture

---
+++ src/mocks/aluno.ts
@@ -0,0 +1,12 @@
+import { Aluno } from "@/interfaces/types";
+
+const aluno: Aluno = {
+    foto: "https://raw.githubusercontent.com/owhenrique/workout-plan-rm/main/public/avatar.jpeg",
+    sigla: "A",
+    nome: "Andre",
+    idade: "28",
+    peso: "106",
+    objetivo: "Emagrecimento"
+}
+
+export default aluno;
