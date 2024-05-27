231adf8b3342536dd8a1111cbbf5cd634ac1607c
owhenrique <me.pauloalmeida@gmail.com>
2024-05-27 19:08:39 -0300

    feat: change workout img

--- src/mocks/treinoB.ts
+++ src/mocks/treinoB.ts
@@ -7,7 +7,7 @@ const treinoB: Treino = {
       { imagem: "https://media.tenor.com/bqKtsSuqilQAAAAM/gym.gif", nome: "Cadeira extensora", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://media.tenor.com/ZElx6PviDq0AAAAM/gym.gif", nome: "Cadeira flexora", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2017/11/smith-full-squat.gif", nome: "Agachamento Smith", series: 3, repeticoes: 12, descanso: 60 },
-      { imagem: "https://www.lyfta.app/thumbnails/11641201.jpg", nome: "Panturilha Smith", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.lyfta.app/_next/image?url=https%3A%2F%2Flyfta.app%2Fimages%2Fexercises%2F11641101.png&w=640&q=10", nome: "Panturilha Smith", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/corrida-na-esteira-inclinada-1.gif", nome: "Esteira", duracao: 15 }
     ]
   };
